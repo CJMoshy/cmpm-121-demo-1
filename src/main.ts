82b492a03c6ccda968617f91722d0606016632de
CJ Moshy <cjmoshy12@gmail.com>
2024-10-09 14:51:01 -0700

    Added a button to the DOM (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "My amazing game";
 document.title = gameName;
 
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
+const main_container = document.createElement("div");
+main_container.className = "container";
+
+
+const main_click_component = document.createElement("button");
+main_click_component.className = "alien";
+main_click_component.textContent = "👾";
+main_container.append(main_click_component);
+
+app.append(main_container)
