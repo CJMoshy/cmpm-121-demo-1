98207b1ad78a00b3fd9f8c40b244de26daea4533
CJ Moshy <cjmoshy12@gmail.com>
2024-10-09 14:53:28 -0700

    Added a counter and tracking for clicks (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,43 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "My amazing game";
 document.title = gameName;
 
+//globals 
+let count = 0;
+
 const main_container = document.createElement("div");
 main_container.className = "container";
-
 
 const main_click_component = document.createElement("button");
 main_click_component.className = "alien";
 main_click_component.textContent = "👾";
 main_container.append(main_click_component);
 
-app.append(main_container)
+main_click_component.addEventListener("click", () => {
+    count += 1;
+    updateCountDisplayMessage();
+});
+
+const counter = document.createElement("div");
+updateCountDisplayMessage();
+counter.className = "counter";
+main_container.append(counter);
+
+/**
+ * this function sets the display for the user of thier current collected ammount
+ */
+function updateCountDisplayMessage(): void {
+    counter.textContent = getCount();
+  }
+  
+  /**
+   * utility function for the count display message
+   * @return {string}
+   */
+  function getCount(): string {
+    if (count === 0) return "0";
+    return count === 1
+      ? `1 alien collected!`
+      : `${count.toString()} aliens collected!`;
+  }
+  
+app.append(main_container);
